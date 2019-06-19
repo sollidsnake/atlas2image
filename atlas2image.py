13cd9d972391af7f241bcb77d4a2797693948bd6
Jesse Nazario <jessenzr@gmail.com>
2019-06-18 21:21:48 -0300

    Name swap

--- atlas2image.py
+++ atlas2image.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 import argparse
 from PIL import Image
@@ -6,6 +8,7 @@ class Atlas2Image:
     img_path: str
     width: int
     height: int
+    name_swap_xy = False
 
     def __init__(self, img_path, width, height, name_x=[], name_y=[]):
         self.img_path = img_path
@@ -41,12 +44,16 @@ class Atlas2Image:
                 name_x = self._get_name(self.name_x, xf)
                 name_y = self._get_name(self.name_y, yf)
 
-                imgs.append(
-                    {
-                        'pos': '%s-%s' % (name_x, name_y),
-                        'img': img.crop((x_init, y_init, x_end, y_end))
-                    }
-                )
+                if self.name_swap_xy:
+                    name_x, name_y = name_y, name_x
+
+                imgs.append({
+                    'pos': '%s-%s' % (
+                        name_x,
+                        name_y,
+                    ),
+                    'img': img.crop((x_init, y_init, x_end, y_end))
+                })
 
         for img in imgs:
             yield img
@@ -82,13 +89,14 @@ if __name__ == '__main__':
     parser.add_argument('--output-path', type=str)
     parser.add_argument('--name-x', type=str)
     parser.add_argument('--name-y', type=str)
+    parser.add_argument('--name-swap-xy', action='store_true', dest='name_swap')
 
     arg = parser.parse_args()
-
 
     name_x = (Atlas2Image.process_name(arg.name_x))
     name_y = (Atlas2Image.process_name(arg.name_y))
 
     atlas = Atlas2Image(arg.img_path, arg.width, arg.height, name_x=name_x, name_y=name_y)
+    atlas.name_swap_xy = arg.name_swap
     atlas.crop()
     atlas.save_img(arg.output_path)
